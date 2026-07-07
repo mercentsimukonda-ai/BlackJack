05c5e67c82cd8f1bfeda44d75456ebba7d0927db
N1355285 <mercent.simukonda2024@my.ntu.ac.uk>
2026-07-07 05:14:37 +0100

    Added comments
    Written a skeleton structure of how the code will run

---
+++ BlackJack.py
@@ -0,0 +1,33 @@
+# Software Name: BlackJack  
+# Author: Mercent Simukonda 
+# License: No license 
+
+# lay out the rules 
+
+# Tell the user how much money they are starting with 
+# Ask the user how much they want to bet 
+# based of how much they bet substract this from the current balance 
+
+# show user a random set of cards for the dealer but one card is censored and total number is hidden 
+# show user a random set of cards and the total number they currently have 
+
+# give the user the option to hit, stand or double down 
+# if the users card is 21 do not give them the option -> show dealers card 
+
+# if user hits another card is added. 
+# if user stands -> show dealers card 
+# if user doubles down -> the bet is doubleded and another card is added 
+# If total is above 21 do not give them an option -> show dealers card
+
+# show dealers card
+
+# if the dealers card is less than 17 they keep hitting until its 17 or more 
+
+# if dealers card > 21 and the users card is more than or equal to 17  but less than or equal to 21 the user wins and the bet is added to their total balance 
+# if dealers card > 21 and the users card is less than 17 or more than 21 the dealer  wins and the bet is removed from the total balance 
+# if dealers card = 21 and the users card is less than 21 or more than 21 the dealer wins and takes the bet 
+# if the dealers card = 21 and the users card is 21 its a tie and the bet goes back to the balance
+# if the dealers card is more than or equal to 17 but less than or equal to 21 and its the same with the user as well The highest card holder wins 
+# if the dealers card is more than or equal to 17 but less than or equal to 21 and its the same with the user but they have the same score the bet is returned to the balance of the user
+
+#give the option for the user to repeat the while loop again or they can exit 
