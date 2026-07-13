bdfaeffe14528e070d7a45900894a45751cdd42a
N1355285 <mercent.simukonda2024@my.ntu.ac.uk>
2026-07-13 15:24:48 +0100

    Changed method from str to repr
    Card deck was not printing nicely as it could not print the object of the unicode characters using the __str__ method

--- BlackJack.py
+++ BlackJack.py
@@ -11,8 +11,8 @@ class Card(object):
         self.value = value 
         self.suit = suit 
         
-    def __str__(self):
-        return f"{self.value} of {self.suit}"
+    def __repr__(self):
+        return f"{self.value}{self.suit}"
 
 
 deck = [Card(value, suit) for value in RANKS for suit in SUITS]
